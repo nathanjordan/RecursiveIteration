6ff16410a701d4c5239bb1f957915193823fe251
Nathan Jordan <natedagreat27274@gmail.com>
2013-10-13 23:30:52 -0700

    started github integration

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+$().ready(function() {
+    // Gets my feed data from GitHub
+    $.ajax({
+        url: 'https://api.github.com/users/nathanjordan/events'
+    }).done(function(data) {
+        console.log(data);
+    });
+});
