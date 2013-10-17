33ac02d3ed2cd88674b30ae49d2bfe973e5cb285
Nathan Jordan <natedagreat27274@gmail.com>
2013-10-17 01:07:31 -0700

    progress

--- js/main.js
+++ js/main.js
@@ -11,4 +11,16 @@ $(document).ready(function() {
     var controller = $.superscrollorama({
         playoutAnimations: true
     });
+    // parallax example
+    controller.addTween(
+      'body',
+      (new TimelineLite())
+        .append([
+          TweenMax.fromTo($('#venn'), 1,
+            {css:{top: 200}, immediateRender:true},
+            {css:{top: -600}})
+        ]),
+      1000 // scroll duration of tween
+    );
 });
+
